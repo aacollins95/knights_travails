a37e165b3e3f59be9ad727b232ff18bc8ce7508c
Aaron Collins <aa.collins.95@gmail.com>
2019-04-10 17:15:57 -0400

    initial commit

---
+++ knights_travails.rb
@@ -0,0 +1,8 @@
+def knight_moves(start,end)
+  #make a board(list(?))
+  #generate tree from root=start and boardlist
+  #bfs on tree for end
+  #profit
+
+  #legality tests, algerbraic to numeric
+end
---
+++ lib/board.rb
@@ -0,0 +1,12 @@
+class Board
+  def initialize
+    @start = coords
+    makeboard #8x8 array
+
+
+  end
+end
+
+class Square
+  def intialize
+end
